7ae058c361c55625938b6102edc54eb7662978c3
Julio Souza <julio.cesar.vgs@gmail.com>
2019-09-21 14:00:16 -0300

    Projeto Kotlin Alura

---
+++ FinancasK/app/src/main/java/alura/com/br/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,13 @@
+package alura.com.br.ui.activity
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
