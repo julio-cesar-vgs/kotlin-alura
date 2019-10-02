40d2995bc2d4fb3aefdebc0457ce0eb442af9898
Sleepk <julio.cesar.vgs@gmail.com>
2019-10-01 23:31:23 -0300

    Ajustando para telas iniciais do projeto alura

---
+++ FinancasK/app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,13 @@
+package com.example.financask.ui.activity
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.financask.R
+
+class ListaTransacoesActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+}
