1ac361dd672aa814c7d07f8c18ecca49a701d7b7
Julio Cesar <julio.cesar.vgs@gmail.com>
2019-10-02 00:24:51 -0300

    Kotlin android - Capitulo 2 - Aula 1 - Criando o modelo para transação
    Signed-off-by: Sleepk <julio.cesar.vgs@gmail.com>

--- FinancasK/app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
+++ FinancasK/app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
@@ -1,13 +1,28 @@
 package com.example.financask.ui.activity
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.financask.R
+import com.example.financask.model.Transacao
+import com.example.financask.ui.adapter.ListaTransacoesAdapter
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import java.math.BigDecimal
+import java.util.*
+
 
 class ListaTransacoesActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
+
+        val transacoes = listOf(
+            Transacao(BigDecimal(20.5), "Comida", Calendar.getInstance()),
+            Transacao(BigDecimal(120.5), "Jornal", Calendar.getInstance())
+        )
+
+
+
+
+        lista_transacoes_listview.adapter = ListaTransacoesAdapter(transacoes, this)
     }
 }
---
+++ FinancasK/app/src/main/java/com/example/financask/ui/adapter/ListaTransacoesAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.financask.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.example.financask.R
+import com.example.financask.model.Transacao
+import kotlinx.android.synthetic.main.transacao_item.view.*
+
+class ListaTransacoesAdapter(
+    transacoes: List<Transacao>,
+    context: Context
+) : BaseAdapter() {
+    private val transacoes = transacoes
+    private val context = context
+
+    /**
+     * Cria um item de navegacao de uma tela para a outra
+     */
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val viewCreate = LayoutInflater.from(context)
+            .inflate(R.layout.transacao_item, parent, false)
+
+        val transacao = transacoes[position]
+
+        viewCreate.transacao_valor.text = transacao.getValor().toString()
+
+        return viewCreate
+    }
+
+    override fun getItem(position: Int) = transacoes.get(position)
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount() = transacoes.size
+}
